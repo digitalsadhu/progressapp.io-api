50ee4c697b87f15ae350787d57453d6ab61b3224
Richard Walker <digitalsadhu@gmail.com>
2015-05-30 22:13:00 +0200

    Initial commit

---
+++ common/models/progress-bar.js
@@ -0,0 +1,19 @@
+module.exports = function(ProgressBar) {
+  // ProgressBar.afterRemote('**', function (ctx, result, next) {
+  //   if(ctx.result) {
+  //     if(Array.isArray(ctx.result)) {
+  //       ctx.result = { 'progressBar': result };
+  //     } else {
+  //       ctx.result = { 'progressBar': result };
+  //     }
+  //   }
+
+  //   next();
+  // });
+
+  // ProgressBar.observe('before validate', function (ctx, next) {
+  //   ctx.instance = ctx.instance.progressBar;
+  //   console.log(ctx.instance);
+  //   next();
+  // });
+};
